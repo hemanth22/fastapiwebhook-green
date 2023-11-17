86ee9f647f01ecc409fb95fe3bb5a51e4827be20
Hemanth B <hemanth22hemu@gmail.com>
2023-11-17 14:52:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ app = FastAPI()
 async def webhook(request: Request):
     content_type = request.headers.get("Content-Type")
     
-    IFTTT_WEBHOOK = os.environ.get('IFTTT_WEBHOOK')
-    base_url = f'https://maker.ifttt.com/trigger/bitroid_hello/with/key/{IFTTT_WEBHOOK}'
-    
     if content_type == "application/json":
         # Handle JSON payload
         payload = await request.json()
@@ -24,14 +21,6 @@ async def webhook(request: Request):
             "value3": payloaddata["source"]
          }
         print("Transformed JSON to client: ", IFTTT_PAYLOAD)
-        try:
-            response = requests.post(base_url, json=IFTTT_PAYLOAD)
-            if response.status_code == 200:
-                print('JSON payload sent successfully to IFTTT Webhook')
-            else:
-                print(f'Failed to send JSON payload. Status code: {response.status_code}')
-        except Exception as e:
-            print(f'An error occurred: {str(e)}')
         
         # Handle the JSON payload as needed
     elif content_type == "application/x-www-form-urlencoded":
